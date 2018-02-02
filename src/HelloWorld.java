995f48d0a5b68d8ab1a27291362df6bec1a9fed2
user <user@user-PC>
2018-02-02 15:23:19 +0800

    test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		System.out.println("111111111111");
+		System.out.println("Hello World!");		
 		
+		System.out.println("the second changes");
 
 	}
 
